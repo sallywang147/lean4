1cdeee67c972bf8c5d11f6616b04848e04e11780
sallywang147 <sallyjunsongwang@gmail.com>
2025-03-24 15:09:41 -0700

    add

---
+++ build/release/stage1/library/CMakeFiles/library.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sally/lean4/src/library/annotation.cpp" "library/CMakeFiles/library.dir/annotation.cpp.o" "gcc" "library/CMakeFiles/library.dir/annotation.cpp.o.d"
+  "/home/sally/lean4/src/library/aux_recursors.cpp" "library/CMakeFiles/library.dir/aux_recursors.cpp.o" "gcc" "library/CMakeFiles/library.dir/aux_recursors.cpp.o.d"
+  "/home/sally/lean4/src/library/bin_app.cpp" "library/CMakeFiles/library.dir/bin_app.cpp.o" "gcc" "library/CMakeFiles/library.dir/bin_app.cpp.o.d"
+  "/home/sally/lean4/src/library/class.cpp" "library/CMakeFiles/library.dir/class.cpp.o" "gcc" "library/CMakeFiles/library.dir/class.cpp.o.d"
+  "/home/sally/lean4/src/library/constants.cpp" "library/CMakeFiles/library.dir/constants.cpp.o" "gcc" "library/CMakeFiles/library.dir/constants.cpp.o.d"
+  "/home/sally/lean4/src/library/dynlib.cpp" "library/CMakeFiles/library.dir/dynlib.cpp.o" "gcc" "library/CMakeFiles/library.dir/dynlib.cpp.o.d"
+  "/home/sally/lean4/src/library/elab_environment.cpp" "library/CMakeFiles/library.dir/elab_environment.cpp.o" "gcc" "library/CMakeFiles/library.dir/elab_environment.cpp.o.d"
+  "/home/sally/lean4/src/library/expr_lt.cpp" "library/CMakeFiles/library.dir/expr_lt.cpp.o" "gcc" "library/CMakeFiles/library.dir/expr_lt.cpp.o.d"
+  "/home/sally/lean4/src/library/formatter.cpp" "library/CMakeFiles/library.dir/formatter.cpp.o" "gcc" "library/CMakeFiles/library.dir/formatter.cpp.o.d"
+  "/home/sally/lean4/src/library/init_module.cpp" "library/CMakeFiles/library.dir/init_module.cpp.o" "gcc" "library/CMakeFiles/library.dir/init_module.cpp.o.d"
+  "/home/sally/lean4/src/library/max_sharing.cpp" "library/CMakeFiles/library.dir/max_sharing.cpp.o" "gcc" "library/CMakeFiles/library.dir/max_sharing.cpp.o.d"
+  "/home/sally/lean4/src/library/module.cpp" "library/CMakeFiles/library.dir/module.cpp.o" "gcc" "library/CMakeFiles/library.dir/module.cpp.o.d"
+  "/home/sally/lean4/src/library/num.cpp" "library/CMakeFiles/library.dir/num.cpp.o" "gcc" "library/CMakeFiles/library.dir/num.cpp.o.d"
+  "/home/sally/lean4/src/library/print.cpp" "library/CMakeFiles/library.dir/print.cpp.o" "gcc" "library/CMakeFiles/library.dir/print.cpp.o.d"
+  "/home/sally/lean4/src/library/profiling.cpp" "library/CMakeFiles/library.dir/profiling.cpp.o" "gcc" "library/CMakeFiles/library.dir/profiling.cpp.o.d"
+  "/home/sally/lean4/src/library/projection.cpp" "library/CMakeFiles/library.dir/projection.cpp.o" "gcc" "library/CMakeFiles/library.dir/projection.cpp.o.d"
+  "/home/sally/lean4/src/library/reducible.cpp" "library/CMakeFiles/library.dir/reducible.cpp.o" "gcc" "library/CMakeFiles/library.dir/reducible.cpp.o.d"
+  "/home/sally/lean4/src/library/replace_visitor.cpp" "library/CMakeFiles/library.dir/replace_visitor.cpp.o" "gcc" "library/CMakeFiles/library.dir/replace_visitor.cpp.o.d"
+  "/home/sally/lean4/src/library/time_task.cpp" "library/CMakeFiles/library.dir/time_task.cpp.o" "gcc" "library/CMakeFiles/library.dir/time_task.cpp.o.d"
+  "/home/sally/lean4/src/library/util.cpp" "library/CMakeFiles/library.dir/util.cpp.o" "gcc" "library/CMakeFiles/library.dir/util.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
