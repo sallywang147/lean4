24bd9a9c599cbcad89e29539021b2be62c53bf86
sallywang147 <sallyjunsongwang@gmail.com>
2025-03-25 04:54:41 -0700

    add dir

---
+++ build/release/stage0/runtime/CMakeFiles/leanrt.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sally/lean4/stage0/src/runtime/alloc.cpp" "runtime/CMakeFiles/leanrt.dir/alloc.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/alloc.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/allocprof.cpp" "runtime/CMakeFiles/leanrt.dir/allocprof.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/allocprof.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/apply.cpp" "runtime/CMakeFiles/leanrt.dir/apply.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/apply.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/compact.cpp" "runtime/CMakeFiles/leanrt.dir/compact.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/compact.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/debug.cpp" "runtime/CMakeFiles/leanrt.dir/debug.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/debug.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/exception.cpp" "runtime/CMakeFiles/leanrt.dir/exception.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/exception.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/hash.cpp" "runtime/CMakeFiles/leanrt.dir/hash.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/hash.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/init_module.cpp" "runtime/CMakeFiles/leanrt.dir/init_module.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/init_module.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/interrupt.cpp" "runtime/CMakeFiles/leanrt.dir/interrupt.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/interrupt.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/io.cpp" "runtime/CMakeFiles/leanrt.dir/io.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/io.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/libuv.cpp" "runtime/CMakeFiles/leanrt.dir/libuv.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/libuv.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/memory.cpp" "runtime/CMakeFiles/leanrt.dir/memory.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/memory.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/mpn.cpp" "runtime/CMakeFiles/leanrt.dir/mpn.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/mpn.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/mpz.cpp" "runtime/CMakeFiles/leanrt.dir/mpz.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/mpz.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/mutex.cpp" "runtime/CMakeFiles/leanrt.dir/mutex.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/mutex.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/object.cpp" "runtime/CMakeFiles/leanrt.dir/object.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/object.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/object_ref.cpp" "runtime/CMakeFiles/leanrt.dir/object_ref.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/object_ref.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/platform.cpp" "runtime/CMakeFiles/leanrt.dir/platform.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/platform.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/process.cpp" "runtime/CMakeFiles/leanrt.dir/process.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/process.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/sharecommon.cpp" "runtime/CMakeFiles/leanrt.dir/sharecommon.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/sharecommon.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/stack_overflow.cpp" "runtime/CMakeFiles/leanrt.dir/stack_overflow.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/stack_overflow.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/stackinfo.cpp" "runtime/CMakeFiles/leanrt.dir/stackinfo.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/stackinfo.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/thread.cpp" "runtime/CMakeFiles/leanrt.dir/thread.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/thread.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/utf8.cpp" "runtime/CMakeFiles/leanrt.dir/utf8.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/utf8.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/uv/event_loop.cpp" "runtime/CMakeFiles/leanrt.dir/uv/event_loop.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/uv/event_loop.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/uv/net_addr.cpp" "runtime/CMakeFiles/leanrt.dir/uv/net_addr.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/uv/net_addr.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/uv/tcp.cpp" "runtime/CMakeFiles/leanrt.dir/uv/tcp.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/uv/tcp.cpp.o.d"
+  "/home/sally/lean4/stage0/src/runtime/uv/timer.cpp" "runtime/CMakeFiles/leanrt.dir/uv/timer.cpp.o" "gcc" "runtime/CMakeFiles/leanrt.dir/uv/timer.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
